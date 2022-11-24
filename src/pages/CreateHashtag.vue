8ad567a87af1db3c059763d856a88fcf45b948f0
Fumiya-Matsumoto <ekiden238@gmail.com>
2022-11-25 04:54:39 +0900

    update hashtag_create_page

--- src/pages/CreateHashtag.vue
+++ src/pages/CreateHashtag.vue
@@ -1,71 +1,254 @@
 <template>
-    <div>
-        <p class="bg-blue-200 text-2xl p-4">ハッシュタグ登録</p>
-        <!-- <div class="grid gap-6 mb-6 md:grid-cols-2 p-4"> -->
-        <!-- </div> -->
-        <div class="px-4 pt-4">
-          <p v-show="error_flag">サーバーとの通信にエラーが発生しています。</p>
-          <div class="pb-4">
-            <label for="hashtag_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">ハッシュタグ名</label>
-            <input v-model='hashtag_name' type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+    <v-container>
+        <h1 
+          class="text-h5 font-weight-bold mb-3 p-3"
+        >ハッシュタグ登録</h1>
+        <v-form
+          :v-model="valid"
+          class="p-3"
+          ref="form"
+        >
+            <v-row v-for="hashtag in hashtagList" :key="hashtag.id">
+            <v-col cols="5">
+              <v-text-field
+                label="ハッシュタグ名"
+                v-model="hashtag.name"
+                required
+                :rules="[v => !!v || 'ハッシュタグ名は必須です', v => !dbRegisterdHashtagList?.includes(v)  || 'ハッシュタグ名は既に登録されています']"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="2">
+              <v-text-field
+                label="投稿数"
+                v-model="hashtag.media_count"
+                required
+                :rules="[v => !!v || '投稿数は必須です']"
+                suffix="件"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="3">
+              <v-select
+                label="ジャンル名"
+                v-model="hashtag.genre_id"
+                :items="genreList"
+                item-title="name"
+                item-value="id"
+              >
+              </v-select>
+            </v-col>
+            <v-col cols="2">
+              <v-btn 
+                dark 
+                small 
+                color="grey" 
+                class="ma-2" 
+                @click="removeInput(hashtag.id)"
+              >
+                <v-icon dark>mdi-minus</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+
+          <v-row justify="center">
+            <v-btn 
+              dark 
+              small 
+              color="grey" 
+              class="ma-2" 
+              @click="addInput()"
+            >
+              <v-icon dark>mdi-plus</v-icon>
+            </v-btn>
+          </v-row>
+
+          <div class="mt-5">
+            <v-btn
+              small
+              color="success"
+              class="mr-4"
+              :loading="loading"
+              @click="submit"
+            >
+              登録
+            </v-btn>
+
+            <v-btn
+              small
+              color="error"
+              @click="reset"
+            >
+              リセット
+            </v-btn>
           </div>
-          <div class="pb-4">
-            <label for="num_post" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">投稿数</label>
-            <input v-model='media_count' type="number" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-          </div>
-          <div class="flex items-start mb-6">
-            <div class="flex items-center h-5">
-              <input id="terms" type="checkbox" value="" class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required>
-            </div>
-            <label for="terms" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">ジャンルを紐付ける</label>
-          </div>
-          <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" @click="createHashtag">登録</button>
-        </div>
+
+          
+        </v-form>
+        
+
         <div class="px-4 pt-4">
           <p>登録したハッシュタグ</p>
           <ul>
-            <li v-for="(hashtag, idx) in hashtags" :key="idx">{{ hashtag }}</li>
+            <li v-for="(hashtag, idx) in registerdHashtagList" :key="idx">{{ hashtag }}</li>
           </ul>
         </div>
-    </div>
+    </v-container>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted, ref } from "vue";
 import axios from 'axios';
 
+interface HashtagInput {
+  id: number
+  name: string
+  media_count: number|null
+  genre_id: string|null
+}
+
+interface Genre {
+  id: string
+  name: string
+  user_id: string
+}
+
+interface Hashtag {
+  id: string
+  name: string
+  media_count: number
+}
+
 export default defineComponent({
   setup() {
-    const hashtag_name = ref();
-    const media_count = ref<number>();
     const error_flag = ref<boolean>(false);
-    const hashtags = ref<string[]>([]);
+    const loading = ref<boolean>(false);
+    const valid = ref<boolean>(true);
+    const form = ref();
+    const dbRegisterdHashtagList = ref<string[]>();
+    const hashtagList = ref<HashtagInput[]>([
+      {
+        id: 0,
+        name: '',
+        media_count: null,
+        genre_id: null
+      }
+    ]);
+    const registerdHashtagList = ref<string[]>([]);
+    const genreList = ref<Genre[]>();
 
     // methods
-    async function createHashtag() {
+    async function getHashtags() {
+      await axios
+        .get('http://localhost:5000/instagram_hashtags')
+        .then(response => dbRegisterdHashtagList.value = response.data.map((hashtag: Hashtag) => hashtag.name))
+    };
+
+    async function getGenres() {
+      await axios
+        .get('http://localhost:5000/instagram_genres')
+        .then(
+          response => genreList.value = response.data.filter(function(genre: Genre){
+            return genre.user_id === '875b3c9e-cb54-4398-97e6-e5010610b960'
+          }) 
+        )
+    };
+
+    async function createHashtag(name: string, media_count: number|null, genre_id: string|null) {
       await axios
         .post('https://influencer-lab-backend.herokuapp.com/instagram_hashtags', {
-          name: hashtag_name.value,
-          media_count: media_count.value
+          name: name,
+          media_count: media_count,
+          instagram_genre_id: genre_id,
         })
         .then((res) => {
           console.log(res);
-          hashtags.value.unshift(res.data.name);
+          registerdHashtagList.value.unshift(res.data.name);
         })
         .catch((err) => {
           console.log(err);
           error_flag.value = true;
         });
+    };
+
+    async function createHashtags() {
+      loading.value = true;
+      await Promise.all(hashtagList.value.map(async (hashtag, i) => {
+        await createHashtag(hashtag.name, hashtag.media_count, hashtag.genre_id)
+        return
+      }))
+      loading.value = false;
+      console.log('終了！')
+    };
+
+    const addInput = () => {
+      hashtagList.value.push(
+        {
+          id: hashtagList.value.length,
+          name: '',
+          media_count: null,
+          genre_id: ''
+        }
+      )
+    };
+
+    const removeInput = (id: number) => {
+      let inputList = hashtagList.value;
+      inputList = inputList.filter((input) => { return input.id !== id;});
+      hashtagList.value = makeNewInput(inputList);
+    };
+
+    const makeNewInput = (inputList: HashtagInput[]) => {
+      let newInputList = [];
+      for (let i = 0; i < inputList.length; i++) {
+        newInputList.push({
+          id: i,
+          name: inputList[i].name,
+          media_count: inputList[i].media_count,
+          genre_id: inputList[i].genre_id
+        });
+      }
+      return newInputList;
     }
+
+    const submit = async (event: any) => {
+      const form_validation = await form.value.validate();
+      console.log(form_validation);
+
+      if (!form_validation.valid) {
+        valid.value = false
+        event.preventDefault();
+        return;
+      } else {
+        createHashtags();
+        getHashtags();
+      }
+    }
+
+    const reset = () => {
+      form.value.reset();
+    }
+
     onMounted(() => {
+      getHashtags()
+      getGenres()
     });
 
     return {
-      hashtag_name,
-      media_count,
       error_flag,
-      hashtags,
+      loading,
+      valid,
+      form,
+      genreList,
+      dbRegisterdHashtagList,
+      hashtagList,
+      registerdHashtagList,
+      getHashtags,
+      getGenres,
       createHashtag,
+      createHashtags,
+      addInput,
+      removeInput,
+      submit,
+      reset
     }
   }
 });
