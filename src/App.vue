5b166367b3ab71f6eef8a2ac6e1e2bd8fad530bb
Fumiya-Matsumoto <ekiden238@gmail.com>
2022-11-25 11:57:40 +0900

    update navigation var

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,10 @@
       <v-layout>
         <v-app-bar>
           <template v-slot:prepend>
-            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            <v-app-bar-nav-icon
+              @click.stop="drawer = !drawer"
+            >
+            </v-app-bar-nav-icon>
           </template>
 
           <v-app-bar-title>InfluencerLab</v-app-bar-title>
@@ -17,24 +20,33 @@
           ></v-btn>
         </v-app-bar>
 
-        <v-navigation-drawer>
+        <v-navigation-drawer
+          v-model="drawer"
+          absolute
+          disable-resize-watcher
+        >
           <v-list
             density="compact"
             nav
           >
-            <v-list-item
-              v-for="(nav_list, idx) in nav_lists"
-              :key="idx"
-              :to="nav_list.link"
+            <v-list-item-group
+              v-model="group"
+              active-class="deep-purple--text text--accent-4"
             >
-              <template v-slot:prepend>
-                <v-icon :icon="nav_list.icon"></v-icon>
-              </template>
+              <v-list-item
+                v-for="(nav_list, idx) in nav_lists"
+                :key="idx"
+                :to="nav_list.link"
+              >
+                <template v-slot:prepend>
+                  <v-icon :icon="nav_list.icon"></v-icon>
+                </template>
 
-              <v-list-item-content>
-                <v-list-item-title v-text="nav_list.name"></v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title v-text="nav_list.name"></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
           </v-list>
         </v-navigation-drawer>
 
@@ -48,12 +60,15 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, watch } from 'vue';
 
 export default defineComponent({
 
   setup() {
     const theme = ref('light')
+    const drawer = ref(false)
+    const group = ref(null)
+
     const nav_lists = [
       {
         name: 'ホーム',
@@ -81,8 +96,14 @@ export default defineComponent({
       theme.value = theme.value === 'light' ? 'dark' : 'light'
     }
 
+    watch(group, () => {
+      drawer.value = false
+    })
+
     return {
       theme,
+      drawer,
+      group,
       nav_lists,
       onClick,
     }
