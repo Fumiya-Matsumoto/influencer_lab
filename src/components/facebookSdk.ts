79793594f8150398184d445edd4b5c85d3f3ceb7
Fumiya-Matsumoto <ekiden238@gmail.com>
2022-12-16 13:17:46 +0900

    add facebook login

---
+++ src/components/facebookSdk.ts
@@ -0,0 +1,102 @@
+export class FacebookSdk {
+    private FACEBOOK_SCRIPT_ID = "facebook-jssdk";
+    private sdkInitialized = false;
+  
+    constructor(private locale: string = "en_US") {
+      this.load();
+    }
+  
+    get srcUrl(): string {
+      return `//connect.facebook.net/${this.locale}/sdk.js`;
+    }
+  
+    private validSdkInitialized() {
+      if (!this.sdkInitialized) {
+        new Error("Sdk is not initialize. Please exec init() function.");
+      }
+    }
+  
+    private load(): void {
+      if (document.getElementById(this.FACEBOOK_SCRIPT_ID)) {
+        // facebookのsdkが読み込まれていたら終了
+        return;
+      }
+  
+      // sdkを読み込むためのscriptタグを生成
+      const scriptElement = document.createElement("script");
+      scriptElement.id = this.FACEBOOK_SCRIPT_ID;
+      scriptElement.src = this.srcUrl;
+      scriptElement.onerror = error => {
+        console.error(`${this.srcUrl} load Fail.`);
+        new Error(error.toString());
+      };
+  
+      // headに仕込む＆読み込みさせる
+      document.querySelector("head")?.appendChild(scriptElement);
+    }
+  
+    init(
+      appId: string,
+      autoLogAppEvents = true,
+      xfbml = true,
+      version = "v8.0"
+    ): Promise<Facebook> {
+      // SDKダウンロード（ダウンロード済みなら何もしないでおわり）
+      this.load();
+  
+      return new Promise<Facebook>((resolve, reject) => {
+        try {
+          window.FB.init({
+            appId,
+            autoLogAppEvents,
+            xfbml,
+            version
+          });
+  
+          resolve(window.FB);
+          this.sdkInitialized = true;
+        } catch (error) {
+          reject(error);
+          this.sdkInitialized = false;
+        }
+      });
+    }
+  
+    getLoginStatus(): void {
+      this.validSdkInitialized();
+  
+      window.FB.getLoginStatus((response: FacebookCallbackResponse) => {
+        console.log({
+          response
+        });
+      });
+    }
+  
+    login(
+      callback: (response: FacebookCallbackResponse) => void,
+      option?: Record<any, any>
+    ): void {
+      this.validSdkInitialized();
+      window.FB.login(callback, option);
+    }
+  
+    logout(): void {
+      this.validSdkInitialized();
+  
+      window.FB.logout((response: FacebookCallbackResponse) => {
+        console.log({
+          response
+        });
+      });
+    }
+  
+    api(
+      path: string,
+      callback: (response: Record<symbol, any>) => void,
+      method: "get" | "post" | "delete" = "get",
+      params?: Record<symbol, any>
+    ): void {
+      this.validSdkInitialized();
+      window.FB.api(path, method, params, callback);
+    }
+  }
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,17 @@
 <template>
-
+    <facebook-login-form></facebook-login-form>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted, ref } from "vue";
-import axios from 'axios';
+import FacebookLoginForm from "../components/FacebookLoginForm.vue";
 
 
 export default defineComponent({
-  
+    name: "Home",
+    components: {
+        FacebookLoginForm
+    }
 })
 
 </script>
